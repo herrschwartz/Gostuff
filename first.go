20f4b681cc121cc59c70dd637eaa9932ad35dbbe
Tim Schwartz <herrschwartz@gmail.com>
2017-10-21 01:17:18 -0700

    some more sillyness

--- first.go
+++ first.go
@@ -4,6 +4,8 @@ import (
 	"os"
 	"fmt"
 	"strconv"
+	"bufio"
+	"strings"
 )
 
 //Everyone loves boilerplate
@@ -39,15 +41,34 @@ func createEntry(s string ){
 	}
 
 	d.Seek(0,2)
-	_, er := d.WriteString("ID:"+ strconv.FormatInt(ID, 10) + " DATA:" +s+ "\n")
+	_, er := d.WriteString( strconv.FormatInt(ID, 10) + "," +s+ "\n")
 	check(er)
 }
 
-func main() {
-for i:=0; i<10; i++{
-	createEntry("test Entry" + string(i))
+func readEntry (id int64) string {
+	d, err := os.OpenFile("data.txt", os.O_RDWR|os.O_CREATE, 0644)
+	check(err)
+	defer d.Close()
+
+	s := bufio.NewScanner(d)
+	s.Split(bufio.ScanLines)
+
+	for s.Scan() {
+		comp := strings.Split(s.Text(), ",")
+		val,_  :=  strconv.ParseInt(comp[0], 10, 64)
+		if id == val {
+			return s.Text()
+		}
+	}
+	return "Error: Not Found"
 }
 
+func main() {
+	for i:=0; i<10; i++ {
+		createEntry("Lets Go!" + strconv.FormatInt(int64(i), 10))
+	}
+	fmt.Println("Enter Somthing: ")
+	fmt.Println(readEntry(5))
 
 
 }
